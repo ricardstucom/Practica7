e4a2ff7c9b3c0496ca80c73c8c7ad302d85e6436
46989075Y <ricardhuelamo@gmail.com>
2017-01-31 20:17:16 +0100

    parte 1JavaScript done, bootstrapp acabado(angular)

--- public_html/practica08_ZSINTESIS_UF2/service.js
+++ public_html/practica08_ZSINTESIS_UF2/service.js
@@ -1,105 +1,103 @@
 
-//
-//
-//
-////
-//function Coche(nombre, precio, extras) {
-//    this.nombre = nombre || "nombre defecto";
-//    this.precio = precio || "20293";
-//    this.extras = extras;
-//}
-//
-//function Concesionario() {
-//    this.nombre;
-//    this.direccion;
-//    this.comanda = new Array();
-//
-//    this.addComanda = function (coche) {
-//        var idComanda = this.comanda.push(coche);
-//        return idComanda;
-//    };
-//}
 
 
 
 
 
-function Jugador(nickname, nom, posicio,punts ){
-    
+function Jugador(nickname, nom, posicio, punts) {
+
     this.nickname = nickname;
     this.nom = nom;
     this.posicio = posicio;
     this.punts = punts;
+
+    this.updateJugador = function (param) {
+        this.nickname = param.nickname || this.nickname;
+        this.nom = param.nom || this.nom;
+        this.posicio = param.posicio || this.posicio;
+        this.punts = param.punts || this.punts;
+    };
+
 }
-function Equipo(){
+function Equipo() {
     this.nom;
     this.victories;
     this.derrotes;
     this.tipusJoc;
-    this.jugadors=[];
-    
-    
-    this.addPlayer = function(jugador){
+    this.jugadors = [];
+
+
+    this.addPlayer = function (jugador) {
 //         this.jugadors[jugador.nom] = jugador;
-         this.jugadors.push(jugador);
+        this.jugadors.push(jugador);
     };
-     this.getPlayer = function(nickname){
-     for(var key in this.jugadors){
-         if((this.jugadors[key].nickname)===(nickname)){
-               return this.jugadors[key];
-         }
-     }
-               
-            
-           
-       
-              
-        
+    this.getPlayer = function (nickname) {
+        for (var key in this.jugadors) {
+            if ((this.jugadors[key].nickname) === (nickname)) {
+                return this.jugadors[key];
+            }
+        }
+
+
+
+
+
+
     };
-     this.delPlayer = function(nickname){
-         for(var i=0;jugadors.length();i++){
-            if(jugadors[i].getNickname().equals(nickname)){
-                
-                
+    this.delPlayer = function (nickname) {
+        for (var i = 0; jugadors.length(); i++) {
+            if (jugadors[i].getNickname().equals(nickname)) {
+
+
                 jugadors.splice(i, 1);
-              
+
             }
         }
     };
-     this.getPlayerMaxPunts = function(){
-        var max= new Jugador("","","",0);
-        for(var key in this.jugadors){
-            if(this.jugadors[key].punts>max.punts){
+    this.getPlayerMaxPunts = function () {
+        var max = new Jugador("", "", "", 0);
+        for (var key in this.jugadors) {
+            if (this.jugadors[key].punts > max.punts) {
                 max = this.jugadors[key];
-                
-                
-                
-              
+
+
+
+
             }
         }
         return max;
     };
+
+
 }
 
 var jugadorPrueba = new Jugador;
 
 jugadorPrueba.nickname = "petu";
-jugadorPrueba.nom="pedro";
-jugadorPrueba.posicio="alero";
-jugadorPrueba.punts=12;
+jugadorPrueba.nom = "pedro";
+jugadorPrueba.posicio = "alero";
+jugadorPrueba.punts = 12;
+
+var jugadorPrueba2 = new Jugador;
+
+jugadorPrueba2.nickname = "petu2";
+jugadorPrueba2.nom = "pedro2";
+jugadorPrueba2.posicio = "alero2";
+jugadorPrueba2.punts = 5;
+
 
 
 
 var equipoPrueba = new Equipo;
 
 
-equipoPrueba.nom="CEEUROPA";
-equipoPrueba.victories=12;
-equipoPrueba.derrotes=9;
-equipoPrueba.tipusJoc="futbol";
+equipoPrueba.nom = "CEEUROPA";
+equipoPrueba.victories = 12;
+equipoPrueba.derrotes = 9;
+equipoPrueba.tipusJoc = "futbol";
 
 equipoPrueba.addPlayer(jugadorPrueba);
-
+equipoPrueba.addPlayer(jugadorPrueba2);
 
 
 
@@ -114,29 +112,12 @@ document.write('<br />Equipo TipusJoc: ');
 document.write(equipoPrueba.tipusJoc);
 document.write('<br />Jugador: ');
 //document.write(equipoPrueba.getPlayer("petu").nom);
-console.log(equipoPrueba.getPlayer("petu"));
-console.log(equipoPrueba.getPlayerMaxPunts());
-//console.log(jugadorPrueba);
+document.write(equipoPrueba.getPlayer("petu").nom);
+document.write('<br />Jugador con más puntos: ');
+document.write(equipoPrueba.getPlayerMaxPunts().punts);
 
-//
-//app.service("serv", function () {
-//  
-//    
-//
-//    this.crearEquipo = function(nom, victories, derrotes, tipusJoc){
-//        var e = new Equipo();
-//        e.nom=nom;
-//        e.victories=victories;
-//        e.derrotes=derrotes;
-//        e.tipusJoc=tipusJoc;
-//        this.equipo.push(e);
-//    };
-//    
-//    /*	Una función que añada un coche a un concesionario.   */
-//    this.addJugadorEquipo= function(num_equipo,nickname,nom,posicio, punts){
-//        var j= new Jugador(nickname, nom, posicio, punts);                
-//        var equip =this.equipos[num_conces];
-//        conces.addComanda(c); //add el coche utilizando la funcion de concesiono addComanda
-//    };
-//
-//});
+jugadorPrueba2.updateJugador({"punts": 30});
+
+document.write('<br />Jugador con más puntos UPDATE:');
+document.write(equipoPrueba.getPlayerMaxPunts().punts);
+
