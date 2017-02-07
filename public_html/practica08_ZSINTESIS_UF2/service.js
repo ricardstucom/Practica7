20d96a755df28d5b28bcb77150f49a0627660f86
46989075Y <ricardhuelamo@gmail.com>
2017-02-07 20:18:29 +0100

    seguir sintesis angular

--- public_html/practica08_ZSINTESIS_UF2/service.js
+++ public_html/practica08_ZSINTESIS_UF2/service.js
@@ -6,56 +6,57 @@
 
 function Jugador(nickname, nom, posicio, punts) {
 
-    this.nickname = nickname;
-    this.nom = nom;
-    this.posicio = posicio;
-    this.punts = punts;
+    this.nickname = nickname ||"";
+    this.nom = nom ||"";
+    this.posicio = posicio||"";
+    this.punts = punts||0;
 
-    this.updateJugador = function (param) {
-        this.nickname = param.nickname || this.nickname;
-        this.nom = param.nom || this.nom;
-        this.posicio = param.posicio || this.posicio;
-        this.punts = param.punts || this.punts;
+    this.updateJugador = function (nom,posicio,punts) {
+        this.nom=nom;
+        this.posicio=posicio;
+        this.punts=punts;
+        return this;
     };
 
 }
 function Equipo() {
-    this.nom;
-    this.victories;
-    this.derrotes;
-    this.tipusJoc;
+    this.nombre = nombre || "";
+    this.victories = victories ||0;
+    this.derrotes=derrotes || 0;
+    this.tipusJoc= tipusJoc ||"";
     this.jugadors = [];
 
 
     this.addPlayer = function (jugador) {
-//         this.jugadors[jugador.nom] = jugador;
+
         this.jugadors.push(jugador);
     };
     this.getPlayer = function (nickname) {
+        var jugador = new Jugador();
         for (var key in this.jugadors) {
             if ((this.jugadors[key].nickname) === (nickname)) {
                 return this.jugadors[key];
             }
         }
 
-
+return jugador;
 
 
 
 
     };
     this.delPlayer = function (nickname) {
-        for (var i = 0; jugadors.length(); i++) {
-            if (jugadors[i].getNickname().equals(nickname)) {
+        for (var key in this.jugadors) {
+            if (this.jugadors[key].nickname == nickname) {
+            this.jugadors.splice(key,1);
 
-
-                jugadors.splice(i, 1);
+              
 
             }
         }
     };
     this.getPlayerMaxPunts = function () {
-        var max = new Jugador("", "", "", 0);
+        var max = new Jugador();
         for (var key in this.jugadors) {
             if (this.jugadors[key].punts > max.punts) {
                 max = this.jugadors[key];
@@ -70,54 +71,97 @@ function Equipo() {
 
 
 }
+var equipo = new Equipo("equipoPrueba",10,8,"prueba");
+var jugadorPrueba = new Jugador("jugador","jugadorNick","pivot",100);
+var jugadorPrueba2= new Jugador("jugador2","jugadorNick2","pivot",100);
+var jugadorPrueba3 = new Jugador("jugador3","jugadorNick3","pivot",100);
 
-var jugadorPrueba = new Jugador;
-
-jugadorPrueba.nickname = "petu";
-jugadorPrueba.nom = "pedro";
-jugadorPrueba.posicio = "alero";
-jugadorPrueba.punts = 12;
-
-var jugadorPrueba2 = new Jugador;
-
-jugadorPrueba2.nickname = "petu2";
-jugadorPrueba2.nom = "pedro2";
-jugadorPrueba2.posicio = "alero2";
-jugadorPrueba2.punts = 5;
-
-
-
-
-var equipoPrueba = new Equipo;
-
-
-equipoPrueba.nom = "CEEUROPA";
-equipoPrueba.victories = 12;
-equipoPrueba.derrotes = 9;
-equipoPrueba.tipusJoc = "futbol";
-
-equipoPrueba.addPlayer(jugadorPrueba);
-equipoPrueba.addPlayer(jugadorPrueba2);
-
-
-
-
-document.write('<br />Nompre Equipo: ');
-document.write(equipoPrueba.nom);
-document.write('<br />Equipo Victories: ');
-document.write(equipoPrueba.victories);
-document.write('<br />Equipo Derrotes: ');
-document.write(equipoPrueba.derrotes);
-document.write('<br />Equipo TipusJoc: ');
-document.write(equipoPrueba.tipusJoc);
-document.write('<br />Jugador: ');
+equipo.addPlayer(jugadorPrueba);
+equipo.addPlayer(jugadorPrueba2);
+equipo.addPlayer(jugadorPrueba3);
+//document.write("Nickname"+ jugadorPrueba.nickname);
+//jugadorPrueba.nickname = "petu";
+//jugadorPrueba.nom = "pedro";
+//jugadorPrueba.posicio = "alero";
+//jugadorPrueba.punts = 12;
+//
+//var jugadorPrueba2 = new Jugador;
+//
+//jugadorPrueba2.nickname = "petu2";
+//jugadorPrueba2.nom = "pedro2";
+//jugadorPrueba2.posicio = "alero2";
+//jugadorPrueba2.punts = 5;
+//
+//
+//
+//
+//var equipoPrueba = new Equipo;
+//
+//
+//equipoPrueba.nom = "CEEUROPA";
+//equipoPrueba.victories = 12;
+//equipoPrueba.derrotes = 9;
+//equipoPrueba.tipusJoc = "futbol";
+//
+//equipoPrueba.addPlayer(jugadorPrueba);
+//equipoPrueba.addPlayer(jugadorPrueba2);
+//
+//
+//
+//
+//document.write('<br />Nompre Equipo: ');
+//document.write(equipoPrueba.nom);
+//document.write('<br />Equipo Victories: ');
+//document.write(equipoPrueba.victories);
+//document.write('<br />Equipo Derrotes: ');
+//document.write(equipoPrueba.derrotes);
+//document.write('<br />Equipo TipusJoc: ');
+//document.write(equipoPrueba.tipusJoc);
+//document.write('<br />Jugador: ');
+////document.write(equipoPrueba.getPlayer("petu").nom);
 //document.write(equipoPrueba.getPlayer("petu").nom);
-document.write(equipoPrueba.getPlayer("petu").nom);
-document.write('<br />Jugador con más puntos: ');
-document.write(equipoPrueba.getPlayerMaxPunts().punts);
+//document.write('<br />Jugador con más puntos: ');
+//document.write(equipoPrueba.getPlayerMaxPunts().punts);
+//
+//jugadorPrueba2.updateJugador({"punts": 30});
+//
+//document.write('<br />Jugador con más puntos UPDATE:');
+//document.write(equipoPrueba.getPlayerMaxPunts().punts);
 
-jugadorPrueba2.updateJugador({"punts": 30});
 
-document.write('<br />Jugador con más puntos UPDATE:');
-document.write(equipoPrueba.getPlayerMaxPunts().punts);
+app.service('datos', function () {
 
+    this.equipos = [];
+    this.equipos.push(equipo);
+
+    this.createEquipo = function (nombre, victories, derrotes, tipusJoc) {
+
+        var t = new Team(nombre, victories, derrotes, tipusJoc);
+        this.equipos.push(t);
+    };
+    this.createJugador = function (nickname, nom, posicio, punts, equipo) {
+
+        var p = new Player(nickname, nom, posicio, punts);
+        console.log(p);
+        if (equipo == null) {
+            console.log("sin equipo");
+        } else {
+            var esta = false;
+            for (key in this.teams[equipo].players) {
+                if(this.equipos[equipo].jugadors[key].nickname == nickname){
+                    p.updatePlayer(nombre,funcion,puntos);
+                    this.teams[team].players[key] = p;
+                    esta = true;
+                }
+            }
+            if(esta == false){
+                this.teams[team].addPlayer(p);
+                console.log("jugador añadido");
+            }else{}
+        }
+    };
+    this.delPlayer =  function (nick,team) {
+        this.teams[team].delPlayer(nick);
+    }
+
+});
---
+++ public_html/wireframe/index.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="newcss.css" rel="stylesheet" type="text/css"/>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script> 
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        
+        <title>diseño 1</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row relleno"></div>
+            <div class="row nav">
+<!--                <div class="col-md-3 avatarGeneral">
+                    <div class="avatar">
+                        
+                    </div>
+                    
+                </div>-->
+<!--                <div class="col-md-6">
+                    <nav class="main-menu">
+                    <ul class="list-inline">
+                        
+                        <li class="col-xs-2 camposMenu"><a>a</a></li>
+                        <li class="col-xs-2 camposMenu"><a>b</a></li>
+                        <li class="col-xs-2 camposMenu"><a>c</a></li>
+                        <li class="col-xs-2 camposMenu"><a>d</a></li>
+                        <li class="col-xs-2 camposMenu"><a>e</a></li>
+                        <li class="col-xs-2 camposMenu"><a>f</a></li>
+                       
+                    </ul>
+                        </nav>
+                </div>
+                
+                <div class="col-md-3">
+                    
+                </div>-->
+
+
+<nav class="navbar navbar-default" role="navigation">
+  <!-- El logotipo y el icono que despliega el menú se agrupan
+       para mostrarlos mejor en los dispositivos móviles -->
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse"
+            data-target=".navbar-ex1-collapse">
+      <span class="sr-only">Desplegar navegación</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+      <a class="navbar-brand" href="#"><img class="avatar" src="STUCOM-mini.png"/></a>
+  </div>
+ 
+  <!-- Agrupar los enlaces de navegación, los formularios y cualquier
+       otro elemento que se pueda ocultar al minimizar la barra -->
+  <div class="collapse navbar-collapse navbar-ex1-collapse">
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Enlace #1</a></li>
+      <li><a href="#">Enlace #2</a></li>
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+          Menú #1 <b class="caret"></b>
+        </a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Acción #1</a></li>
+          <li><a href="#">Acción #2</a></li>
+          <li><a href="#">Acción #3</a></li>
+          <li class="divider"></li>
+          <li><a href="#">Acción #4</a></li>
+          <li class="divider"></li>
+          <li><a href="#">Acción #5</a></li>
+        </ul>
+      </li>
+    </ul>
+ 
+    <form class="navbar-form navbar-left" role="search">
+      <div class="form-group">
+        <input type="text" class="form-control" placeholder="Buscar">
+      </div>
+      <button type="submit" class="btn btn-default">Enviar</button>
+    </form>
+ 
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#">Enlace #3</a></li>
+      <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+          Menú #2 <b class="caret"></b>
+        </a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Acción #1</a></li>
+          <li><a href="#">Acción #2</a></li>
+          <li><a href="#">Acción #3</a></li>
+          <li class="divider"></li>
+          <li><a href="#">Acción #4</a></li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+
+            </div>
+            
+            
+            <div class="row relleno2"></div>
+            <div class="row imagenes">
+                <div class="col-lg-3"><img class="photo" src="1.jpg"/></div>
+                <div class="col-lg-3"><img class="photo" src="media/2.jpg"/></div>
+                <div class="col-lg-3"><img class="photo" src="media/3.jpg"/></div>
+                <div class="col-lg-3"><img class="photo" src="media/4.jpg"/></div>
+                
+            </div>
+            <div class="row relleno2"></div>
+            <div class="row imagenes">
+                <div class="oferta"> O F E R T A</div>
+                
+            </div>
+            <div class="row relleno2"></div>
+            <div class="row imagenes">
+                <div class="col-lg-3"><img class="photo" src="media/5.jpg"/></div>
+                <div class="col-lg-3"><img class="photo" src="media/2.jpg"/></div>
+                <div class="col-lg-3"><img class="photo" src="media/4.jpg"/></div>
+                <div class="col-lg-3"><img class="photo" src="media/1.jpg"/></div>
+                
+            </div>
+            <div class="row relleno2"></div>
+            <div class="row imagenes">
+                <div class="oferta"> O F E R T A</div>
+                
+            </div>
+        </div>
+    </body>
+</html>
